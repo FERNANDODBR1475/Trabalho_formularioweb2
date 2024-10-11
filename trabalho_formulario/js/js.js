48660ff68fbfe0bf2de52b86895e0048f2043372
FERNANDODBR1475 <132899156+FERNANDODBR1475@users.noreply.github.com>
2024-10-11 09:57:18 -0300

    Add files via upload

---
+++ trabalho_formulario/js/js.js
@@ -0,0 +1,11 @@
+
+let nome = document.getElementById('nome').value;
+let cpf = document.getElementById('cpf').value;
+let celular = document.getElementById('celular').value;
+let email = document.getElementById('email').value;
+let endereco = document.getElementById('endereco').value;
+
+let btnenviar = document.getElementById('enviar');
+btnenviar.addEventListener("click",);
+
+
